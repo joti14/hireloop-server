2f7349e72ebae2535178479137ee99fa84ebb3c0
Joti <hasnahenajoti1414@gmail.com>
2026-06-11 21:58:57 +0600

    Add GET and POST /api/jobs endpoints for job management

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 5000;
 require('dotenv').config()
+
+app.use(cors());
+app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -25,6 +29,30 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("hireloop_db");
+    const jobsCollection = database.collection("jobs");
+    const companyCollection = database.collection('companies');
+
+    app.get('/api/jobs', async(req, res) => {
+        const query = {};
+        if(req.query.companyId){
+            query.companyId = req.query.companyId;
+        }
+        if(req.query.companyId){
+            query.status = req.query.status;
+        }
+        const cursor = jobsCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.post('/api/jobs', async (req, res) => {
+        const job = req.body;
+        const result = await jobsCollection.insertOne(job);
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
